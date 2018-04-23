b7cdd12df3e0577ac0d231a2a550d52a938f2a2b
aalluhaibi <aalluhaibi@WS10TC11107.dbq.int>
2018-04-23 09:59:42 -0500

    chpter12

---
+++ c12Examples/src/c12Examples/c12Main.java
@@ -0,0 +1,53 @@
+/**
+ *   file: c12Main.java
+ */
+package c12Examples;
+
+/**
+ * @author atmanning - atmanning@dbq.edu
+ *
+ */
+public class c12Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		// convert Chapter 12 examples to JPanel or JFrame equivalent
+		
+		System.out.println("Malik Chapter 12 examples.");
+		
+		/*
+		 * new drawFontsJPanel_12_1().setVisible(true);
+		 
+
+		new labelColorsJFrame12_2().setVisible(true);
+		
+		new chapter12_3_drawtext().setVisible(true);
+		
+		new chapter12_4_drawRectangle().setVisible(true);
+		
+		new chapter12_5_drawShapes().setVisible(true);
+		
+		new chapter12_Exercize_17_circles().setVisible(true);
+		
+		new chapter12_Exercize_19_PizzaOrders().setVisible(true);
+
+		new chapter12_Example_FlowLayout().setVisible(true);
+		
+		new chapter12_Example_BorderLayout().setVisible(true);
+		
+		/*
+		 * new chapter12_Example_MenuTextEditor().setVisible(true);
+		 
+		
+		new chapter12_8_OneChar_KeyEvents().setVisible(true);
+		
+		new chapter12_9_MouseExample().setVisible(true);
+		*/
+		
+		new chapter12_Exercize_19_PizzaMenu().setVisible(true);
+	}
+
+}
---
+++ c12Examples/src/c12Examples/labelsJPanel.java
@@ -0,0 +1,17 @@
+/**
+ *   file: labelsJPanel.java
+ */
+package c12Examples;
+
+import javax.swing.*;
+
+/**
+ * @author Abdulelah Alluhaibi
+ *
+ */
+public class labelsJPanel extends JPanel {
+	labelsJPanel() {
+		
+	}
+
+}
